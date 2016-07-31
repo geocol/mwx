f1918ef539bea7d8f3543c3b3361215310af8ff7
Wakaba <wakaba@suikawiki.org>
2016-07-31 23:06:05 +0900

    Added a variant of extraction script that simply invokes existing wpserver

---
+++ bin/extract-from-pages-remote.pl
@@ -0,0 +1,90 @@
+use strict;
+use warnings;
+use Path::Tiny;
+use Encode;
+use Getopt::Long;
+use AnyEvent;
+use Promise;
+use Promised::File;
+use Web::UserAgent::Functions qw(http_post);
+use Wanage::URL;
+use JSON::PS;
+
+sub get_json ($$$$$) {
+  my ($url_prefix, $p1, $p2, $p3, $rules) = @_;
+  return Promise->new (sub {
+    my ($ok, $ng) = @_;
+    http_post
+        url => (sprintf q<%s/%s/%s/%s/extracted.json>,
+                    $url_prefix,
+                    percent_encode_c $p1,
+                    percent_encode_c $p2,
+                    percent_encode_c $p3),
+        params => {
+          rules => $rules
+        },
+        timeout => 1000,
+        anyevent => 1,
+        cb => sub {
+          my $res = $_[1];
+          if ($res->code == 200) {
+            $ok->(json_bytes2perl $res->content);
+          } elsif ($res->code == 404) {
+            $ok->({});
+          } else {
+            $ng->($res->code);
+          }
+        };
+  });
+} # get_json
+
+my $RulesFileName;
+my $URLPrefix;
+GetOptions (
+  '--rules-file-name=s' => \$RulesFileName,
+  '--url-prefix=s' => \$URLPrefix,
+) or die "Usage: $0 [OPTIONS] page-name...\n";
+
+my @page = map { decode 'utf-8', $_ } @ARGV;
+@page or die "Usage: $0 [OPTIONS] page-name...\n";
+
+my $root_path = path (__FILE__)->parent->parent->absolute;
+
+my $p1 = 'p';
+my $p2 = 'ja';
+
+my ($rules_path, $rules_file);
+if (defined $RulesFileName) {
+  $rules_path = path ($RulesFileName);
+  $rules_file = Promised::File->new_from_path ($rules_path);
+}
+
+my $cv = AE::cv;
+Promise->all ([
+  defined $rules_file ? $rules_file->read_char_string : undef,
+])->then (sub {
+  my $rules = $_[0]->[0];
+
+  my $data = {};
+
+  my $get; $get = sub {
+    my $page = shift @page or return;
+    return get_json ($URLPrefix, $p1, $p2, $page, $rules)->then (sub {
+      my $json = $_[0];
+      $data->{$page} = $json;
+      return $get->();
+    });
+  }; # $get
+
+  return $get->()->then (sub {
+    print perl2json_bytes_for_record $data;
+  });
+})->then (sub {
+  return $mwx->stop;
+})->then (sub {
+  $cv->send;
+}, sub {
+  $cv->croak ($_[0]);
+});
+
+$cv->recv;
